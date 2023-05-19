f7fbfb6f266a4a1e37420c5840a5af45de69f870
RaulTarazona <rauldiaz0530@gmail.com>
2023-05-19 08:00:44 -0500

    perf: :zap:

---
+++ Banco.php
@@ -0,0 +1,25 @@
+<?php 
+abstract class Banco {
+    public $nombre = "Davivienda";
+
+    public function getNombre(){
+        return $this -> nombre;
+    }
+
+}
+
+// $banco = new Banco();
+// echo $banco -> getNombre();
+
+class Sucursal extends Banco{
+
+    public $direccion = "Calle 48 #12";
+
+    public function getDireccion(){
+        return $this->direccion;
+    }
+}
+$sucursal = new Sucursal();
+echo $sucursal->getNombre() . "<br>";
+echo $sucursal->getDireccion();
+?>
